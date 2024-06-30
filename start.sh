d4b283e65247d9a0c4f7d97221a288742e1d2061
k0baya <132335755+k0baya@users.noreply.github.com>
2024-06-30 13:52:24 +0800

    Update file path

--- start.sh
+++ start.sh
@@ -8,6 +8,7 @@ export UUID=''
 
 USERNAME=$(whoami)
 WORKDIR="/home/${USERNAME}/sing-box"
-[ ! -e ${WORKDIR}/entrypoint.sh ] && wget https://raw.githubusercontent.com/k0baya/sb-for-serv00/main/entrypoint.sh -O entrypoint.sh && chmod +x entrypoint.sh
-nohup node app.js 2>/dev/null 2>&1 &
+[ ! -e ${WORKDIR}/entrypoint.sh ] && wget https://raw.githubusercontent.com/k0baya/sb-for-serv00/main/entrypoint.sh -O ${WORKDIR}/entrypoint.sh && chmod +x ${WORKDIR}/entrypoint.sh
+[ ! -e ${WORKDIR}/app.js ] && wget https://raw.githubusercontent.com/k0baya/sb-for-serv00/main/app.js -O ${WORKDIR}/app.js
+nohup node ${WORKDIR}/app.js 2>/dev/null 2>&1 &
 sleep 5 && cat ${WORKDIR}/list

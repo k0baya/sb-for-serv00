f78f05ff7ec614acf3222801d349c691a287d9c4
saika2077 <132335755+k0baya@users.noreply.github.com>
2024-08-10 16:21:34 +0800

    Add all-in-one branch

--- README.md
+++ README.md
@@ -5,7 +5,9 @@
 
 * 无需自备域名，使用 Serv00 自带的域名实现 TLS ；
 
-* 同时支持 Hysteria2 和 Tuic5 双协议；
+* 同时支持 Hysteria2 和 Tuic5 、Vless-ws-tls 协议，共 6 种接入方式；
+
+* 支持 Cloudflare CDN 连入；
 
 * JS 定时保活核心
 
@@ -17,38 +19,61 @@
 
 #### 准备工作
 
-首先在 Panel 中放行两个类型为 UDP 端口，并在 Additional services 选项卡中找到 Run your own applications 项目，将其设置为 Enabled 。
+首先你需要至少拥有 1 个托管在 Cloudflare 的域名。
 
-然后是最重要的部分，生成一个 Let's Encrypted 证书：
+然后参考[群晖套件：Cloudflare Tunnel 内网穿透中文教程 支持DSM6、7](https://imnks.com/5984.html) 的教程，在 Cloudflare 控制面板中创建 1 个 Argo Tunnel，把其中 ey 开头的一串 Token 记录下来备用。
 
-在 Panel 中点击左侧菜单栏中的 SSL ，然后点击上方菜单栏中的 WWW websites ，点击第一个 IP Address 最右侧的 Manage 按钮，再点击上方菜单栏中的 Add certificate 按钮，Type 选择 Generate Let's Encrypted certificate， Domain任选一个即可，最后点击下方的 Add 按钮进行生成。请至少保证自己的 Serv00 账号下有一个 Let's Encrypted 的证书。
+同时你还需要 1 个 Serv00 的账号。
 
-接着进入 File manager，在用户目录下新建一个名为`sing-box`的文件夹用于部署 sb-for-Serv00，并将本仓库的文件都上传到此文件夹内。
+然后是最重要的部分，生成一个 Let's Encrypt 证书：
 
-右键点击 `start.sh` 文件，选择 View/Edit > Source Editor ，进行编辑，在 1 - 7 行修改环境变量：
-|变量名|是否必须|默认值|备注|
-|-|-|-|-|
-|HY2PORT|是||Hysteria2 协议监听端口|
-|TUIC5PORT|是||Tuic5 协议监听端口|
-|SERV00PASSWORD|是||你的 Serv00 账号的密码，用于获取 SSL 证书|
-|UUID|否|de04add9-5c68-8bab-950c-08cd5320df18||
+在 Panel 中点击左侧菜单栏中的 SSL ，然后点击上方菜单栏中的 WWW websites ，点击第一个 IP Address 最右侧的 Manage 按钮，再点击上方菜单栏中的 Add certificate 按钮，Type 选择 Generate Let's Encrypt certificate， Domain任选一个即可，最后点击下方的 Add 按钮进行生成。**请至少保证自己的 Serv00 账号下有一个 Let's Encrypt 的证书，否则无法使用本仓库！**
+
+>友情提示，自己的域名添加 A 类型 DNS 记录指向 Serv00 的服务器后，也可以使用 Serv00 的面板内置的功能添加 Let's Encrypt 的证书，且对本仓库的运行同样有效。同时，**自己的域名不受 Serv00 自带域名申请 SSL 证书时的每周数量限制。**
+
+#### 部署 sb-for-Serv00
+
+SSH 登录 Serv00，输入以下命令：
+```shell
+devil binexec on && killall -u $(whoami)
+```
+接着断开 SSH 并重新连接，输入以下命令：
+```shell
+bash <(curl -Ls https://raw.githubusercontent.com/k0baya/sb-for-serv00/all-in-one/entrypoint.sh)
+```
+并按照提示输入相关信息。
+
+>**注意**
+>
+>其中 `ARGO_AUTH`、`ARGO_DOMAIN`、`SERV00PASSWORD` 3 个变量是必须的，其他变量根据需要填写。非必须的变量可以按回车跳过，使用默认值。
+>
+>`ARGO_AUTH` 为上述的 ey 开头的一串 Token。
+>
+>`ARGO_DOMAIN` 为 Vless 协议通过 Cloudflare CDN 回源的域名，请使用你域名的子域进行设置，如下图：
+>
+>![](/pic/argo.png)
+
+
 
 #### 启动并获取配置
 
-SSH 登录 Serv00 ，进入 `start.sh` 所在的路径，直接执行即可启动。
-
-```
-chmod +x start.sh && bash start.sh
-```
-等待程序完成启动，会在 Terminal 中直接打印出 Hysteria2 和 Tuic5 的配置链接。
+按照脚本提示进入 `/status` 的网页，并尝试刷新页面，直到进程列表中出现了包含 `sing-box` 以及 `cloudflared` 字样的进程，就代表 Sb-for-Serv00 已经启动成功。此时你就可以通过访问 `/list` 路径查看到 Sb-for-Serv00 所提供的配置链接了。
+![](/pic/process.png)
 
 ### 自动启动
 
-听说 Serv00 的主机会不定时重启，所以需要添加自启任务。
+Sb-for-Serv00 可以通过访问网页对项目进行唤醒，如果你需要保活，可以使用以下公共服务对网页进行监控：
 
-在 Panel 中找到 Cron jobs 选项卡，使用 Add cron job 功能添加任务，Specify time 选择 After reboot，即为重启后运行。Form type 选择 Advanced，Command 写 `start.sh` 文件的绝对路径，比如：
+1 [cron-job.org](https://console.cron-job.org)
 
-```
-/home/username/sing-box/start.sh
-```
-> 务必按照你的实际路径进行填写。
+2 [UptimeRobot](https://uptimerobot.com/) 
+
+同时，你也可以选择自建 [Uptime-Kuma](https://github.com/louislam/uptime-kuma) 等服务进行监控。
+
+>建议监控 `/info` 路径，因为该路径无需身份验证。
+>
+>不要监控根路径，因为根路径为静态页面，只是该项目的伪装，无法起到保活效果。
+
+### 常见问题
+1. 已知部分 Server （目前已知 s7 和 s8）已经使用 hosts 屏蔽了 Cloudflared 客户端的下载地址，可以通过手动上传二进制文件的方法解决，具体参照：[#19](https://github.com/k0baya/X-for-serv00/issues/19#issuecomment-2266315320)
+2. 已知部分客户端（如 V2rayNG）可能出现导入配置识别不正确的情况，如 vless 协议 `ws path` 应为 `/serv00-vl` ，如果客户端识别不完整，通过手动补全即可正常使用。
--- app.js
+++ app.js
@@ -1,4 +1,218 @@
-var exec = require("child_process").exec;
+require('dotenv').config();
+const process = require('process');
+const username = process.env.WEB_USERNAME || "admin";
+const password = process.env.WEB_PASSWORD || "password";
+const UUID = process.env.UUID || "de04add9-5c68-8bab-950c-08cd5320df18";
+const os = require('os');
+const path = require('path');
+const express = require("express");
+const fs = require('fs');
+const { exec } = require('child_process');
+const crypto = require('crypto');
+const auth = require("basic-auth");
+
+const app = express();
+
+const csrfTokens = new Set();
+
+function generateCSRFToken() {
+    return crypto.randomBytes(16).toString('hex');
+}
+
+function validateCSRFToken(req, res, next) {
+    const token = req.headers['csrf-token'];
+    if (csrfTokens.has(token)) {
+        csrfTokens.delete(token);
+        next();
+    } else {
+        res.status(403).send('请刷新后重试');
+    }
+}
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    const user = auth(req);
+
+    if (req.path === '/info' || req.path.startsWith(`/${UUID}`)) {
+        return next();
+    }
+
+    if (user && user.name === username && user.pass === password) {
+        return next();
+    }
+    res.set("WWW-Authenticate", 'Basic realm="Node"');
+    return res.status(401).send();
+});
+
+const USERNAME = os.userInfo().username;
+const USERNAME_DOMAIN = USERNAME.toLowerCase().replace(/[^a-z0-9-]/g, '');
+const WORKDIR = path.join('/home', USERNAME, 'domains', `${USERNAME_DOMAIN}.serv00.net`, 'public_nodejs');
+process.chdir(WORKDIR);
+
+app.get("/info", function (req, res) {
+    res.type("html").send("<pre>Powered by X-for-Serv00\nAuthor: <a href='https://github.com/k0baya'>K0baya</a>" + "</pre>");
+});
+
+app.get(`/${UUID}/sub`, function (req, res) {
+    let cmdStr = "cat sub";
+    exec(cmdStr, function (err, stdout, stderr) {
+        if (err) {
+            res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+        } else {
+            res.send(stdout);
+        }
+    });
+});
+
+app.get("/status", function (req, res) {
+    let cmdStr = "ps aux";
+    exec(cmdStr, function (err, stdout, stderr) {
+        if (err) {
+            res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+        } else {
+            res.type("html").send("<pre>获取系统进程表：\n" + stdout + "</pre>");
+        }
+    });
+});
+
+app.get("/list", async function (req, res) {
+    let cmdStr = "cat list";
+    const fileExists = (path) => {
+        return new Promise((resolve, reject) => {
+            fs.access(path, fs.constants.F_OK, (err) => {
+                resolve(!err);
+            });
+        });
+    };
+
+    const waitForFile = async (path, retries, interval) => {
+        for (let i = 0; i < retries; i++) {
+            if (await fileExists(path)) {
+                return true;
+            }
+            await new Promise(resolve => setTimeout(resolve, interval));
+        }
+        return false;
+    };
+
+    const fileReady = await waitForFile('list', 30, 1000);
+
+    if (!fileReady) {
+        res.type("html").send("<pre>文件未生成</pre>");
+        return;
+    }
+
+    exec(cmdStr, function (err, stdout, stderr) {
+        if (err) {
+            res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+        } else {
+            const fullUrl = `${req.protocol}://${req.get('host')}/${UUID}/sub`;
+            res.type("html").send("<pre>订阅地址：" + fullUrl + "\n\n节点数据：\n\n" + stdout + "</pre>");
+        }
+    });
+});
+
+app.get('/control', (req, res) => {
+    const csrfToken = generateCSRFToken();
+    csrfTokens.add(csrfToken);
+    
+    const htmlContent = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Control</title>
+        <style>
+            button {
+                margin: 20px;
+                padding: 10px 20px;
+                font-size: 16px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Control Panel</h1>
+        <button onclick="restart()">Restart</button>
+        <button onclick="uninstall()">Uninstall</button>
+
+        <script>
+            const csrfToken = '${csrfToken}';
+
+            function restart() {
+                fetch('/restart', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'CSRF-Token': csrfToken
+                    },
+                    body: JSON.stringify({ action: 'restart' })
+                })
+                .then(response => response.text())
+                .then(message => alert(message))
+                .catch(error => console.error('Error:', error));
+            }
+
+            function uninstall() {
+                fetch('/uninstall', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'CSRF-Token': csrfToken
+                    },
+                    body: JSON.stringify({ action: 'uninstall' })
+                })
+                .then(response => response.text())
+                .then(message => alert(message))
+                .catch(error => console.error('Error:', error));
+            }
+        </script>
+    </body>
+    </html>
+    `;
+    res.type('html').send(htmlContent);
+});
+
+app.post('/restart', validateCSRFToken, (req, res) => {
+    exec('killall -u $(whoami)', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`执行命令错误: ${error}`);
+            return res.status(500).send('重启失败，请检查服务器日志。');
+        }
+        res.send('已经成功重启，请刷新页面。');
+    });
+});
+
+app.post('/uninstall', validateCSRFToken, (req, res) => {
+    const cleanupCmd = `find . -mindepth 1 -maxdepth 1 ! -name 'public' ! -name 'tmp' -exec rm -rf {} + && killall -u $(whoami)`;
+    exec(cleanupCmd, { cwd: WORKDIR }, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`执行命令错误: ${error}`);
+            return res.status(500).send('删除失败，请检查服务器日志。');
+        }
+        res.send('已成功卸载！');
+    });
+});
+  
+function keep_argo_alive() {
+  exec("pgrep -laf cloudflared", function (err, stdout, stderr) {
+    // 1.查后台系统进程，保持唤醒
+    if (stdout.includes("./cloudflared tunnel")) {
+      console.log("Argo 正在运行");
+    } else {
+      //Argo 未运行，命令行调起
+      exec("bash argo.sh 2>&1 &", function (err, stdout, stderr) {
+        if (err) {
+          console.log("保活-调起Argo-命令行执行错误:" + err);
+        } else {
+          console.log("保活-调起Argo-命令行执行成功!");
+        }
+      });
+    }
+  });
+}
+setInterval(keep_argo_alive, 30 * 1000);
 
 function keep_web_alive() {
   exec("pgrep -laf sing-box", function (err, stdout, stderr) {
@@ -6,12 +220,12 @@ function keep_web_alive() {
       console.log("sing-box 正在运行");
     } else {
       exec(
-        "nohup bash entrypoint.sh 2>/dev/null 2>&1 &",
+        "chmod +x sing-box && ./sing-box run -c ./config.json > /dev/null 2>&1 &",
         function (err, stdout, stderr) {
           if (err) {
-            console.log("保活-调起 sing-box 命令执行执行错误:" + err);
+            console.log("保活-调起web-命令行执行错误:" + err);
           } else {
-            console.log("保活-调起 sing-box 命令行执行成功!");
+            console.log("保活-调起web-命令行执行成功!");
           }
         }
       );
@@ -20,10 +234,4 @@ function keep_web_alive() {
 }
 setInterval(keep_web_alive, 10 * 1000);
 
-exec("bash entrypoint.sh", function (err, stdout, stderr) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(stdout);
-});
+app.listen(3000, () => console.log(`Example app listening on port 3000!`));
